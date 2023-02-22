8e6d757ab46057559bb696e14773a251e4d0487d
carlos calderon <cdcalderon@gmail.com>
2023-02-22 16:06:11 -0600

    add get all simulations

---
+++ src/models/simulation.model.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+export interface Simulation {
+  id: string;
+}
+
+export enum SimulationStatus {
+  COMPLETE = 'COMPLETE',
+  IN_PROGRESS = 'IN_PROGRESS',
+}
--- src/simulations/simulations.controller.ts
+++ src/simulations/simulations.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Simulation } from 'src/models/simulation.model';
 import { SimulationsService } from './simulations.service';
 
 @Controller('simulations')
 export class SimulationsController {
   constructor(private simulaitonsService: SimulationsService) {}
+
+  @Get()
+  getAllSimulations(): Simulation[] {
+    return new Array<Simulation>();
+  }
 }
