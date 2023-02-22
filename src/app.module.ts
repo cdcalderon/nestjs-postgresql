0fad14550e289f72b62f5af8e5a40c5a07862854
carlos calderon <cdcalderon@gmail.com>
2023-02-22 15:37:55 -0600

    add simulation controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SimulationModule } from './simulation/simulation.module';
+import { SimulationsController } from './simulations/simulations.controller';
+import { SimulationsService } from './simulations/simulations.service';
 
 @Module({
   imports: [SimulationModule],
+  controllers: [SimulationsController],
+  providers: [SimulationsService],
 })
 export class AppModule {}
