a2091656e846516128d2e2944a6713d89c015932
carlos calderon <cdcalderon@gmail.com>
2023-02-23 20:12:02 -0600

    add validator

--- src/simulations/dtos/create-simulation.dto.ts
+++ src/simulations/dtos/create-simulation.dto.ts
@@ -1,4 +1,8 @@
 /* eslint-disable prettier/prettier */
+import { IsNotEmpty } from 'class-validator';
+
+/* eslint-disable prettier/prettier */
 export class CreateSimulation {
+  @IsNotEmpty()
   name: string;
 }
