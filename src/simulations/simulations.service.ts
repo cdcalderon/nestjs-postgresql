b4dbb4fb47054fc03617b7fc6a79971d46d00971
carlos calderon <cdcalderon@gmail.com>
2023-02-25 21:50:54 -0600

    implement getSimulationById in service

--- src/simulations/simulations.service.ts
+++ src/simulations/simulations.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Simulation } from 'src/entity/simulation.entity';
+import { SimulationRepository } from 'src/repositories/simulation.repository';
+import { FindOneOptions } from 'typeorm';
 
 @Injectable()
 export class SimulationsService {
+	constructor(
+		@InjectRepository(SimulationRepository)
+		private simulationRepository: SimulationRepository,
+	) {}
+
+	async getSimulationById(id: string): Promise<Simulation> {
+		const options: FindOneOptions<Simulation> = {
+			where: { id },
+		};
+		const simulation = await this.simulationRepository.findOne(options);
+
+		if (!simulation) {
+			throw new NotFoundException(`Simulation with ID ${id} not found`);
+		}
+
+		return simulation;
+	}
+
 	getAllSimulations(): Simulation[] {
 		return new Array<Simulation>();
 	}
